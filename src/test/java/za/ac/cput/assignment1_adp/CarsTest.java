39c97779edadd7971174c9d3d95fdd26b7e67079
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-04-01 00:25:44 +0200

    Update and rename ProgramTest.java to CarsTest.java

---
+++ src/test/java/za/ac/cput/assignment1_adp/CarsTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assignment1_adp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CPUT
+ */
+public class CarsTest {
+    
+    public ProgramTest() {
+        private Cars car1;
+        private Cars car2;
+        private Cars car3;    
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+        car1 = new Cars();
+        car2 = new Cars();
+        car3 = car1;
+    }
+    
+    @AfterEach
+    public void tearDown() {
+       car1.delete;
+        car2.delete; 
+        car3.delete; 
+    }
+
+    @Test
+    public void testID() {
+        // TODO review the generated test code and remove the default call to fail.
+        assertSame(car1,car3);
+    }
+    
+     @Test
+    public void testEq() {
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(car1,car3);
+    }
+    
+     @Test
+    public void testFail() {
+        // TODO review the generated test code and remove the default call to fail.
+        assertsEquals(car1.car2);
+        fail("The test will delibearately fail.");
+        System.out.println("The test will delibearately fail");
+    }
+    
+     @Test
+     @Timeout(6)
+    public void testTimeout() {
+        // TODO review the generated test code and remove the default call to fail.
+         Thread.sleep(5000);
+        System.out.println("Test passed within the time"); 
+    }
+    
+     @Test
+    @Disabled ("This is disabled for now")
+    public void testDisabled() {
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(car1,car3);
+    }
+    
+}
